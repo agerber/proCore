afe48c4c9672f870eed5eab090b29d8b0d68294f
Adam Gerber <adam.gerber@yahoo.com>
2026-09-21 10:18:17 -0500

    update pom

--- src/java/_08final/mvc/model/Asteroid.java
+++ src/java/_08final/mvc/model/Asteroid.java
@@ -65,7 +65,8 @@ public class Asteroid extends Sprite {
 	//converts the radius ratio to integer representing the size of the Asteroid (0,1,2)
 	//0 = large, 1 = medium, 2 = small
 	public int getSize() {
-		int ratio = LARGE_RADIUS / getRadius(); // 1, 2, or 4
+		int ratio = LARGE_RADIUS / getRadius(); // ratio will resolve to 1, 2, or 4
+		
 		//ratio = 1 → binary ...0000 0001 → trailing zeros: 0
 		//ratio = 2 → binary ...0000 0010 → trailing zeros: 1
 		//ratio = 4 → binary ...0000 0100 → trailing zeros: 2
